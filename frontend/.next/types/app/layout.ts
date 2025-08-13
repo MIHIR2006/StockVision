04cb3d2848955ed10da31e45065cf0d2889c6bce
adesh019 <adeshwaje0019@gmail.com>
2025-08-13 12:10:56 +0530

    feat:adds sidebar to landing page

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Mihir\OneDrive\Desktop\StockVision\frontend\app\layout.tsx
+// File: C:\Users\admin\Desktop\New folder\StockVision\frontend\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
