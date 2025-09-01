7fdc7c7ee886ac37ae75f16addf30aa7fb951355
MIHIR2006 <mihirgoswami2006@gmail.com>
2025-09-01 10:05:34 +0530

    StockVision Figlet Banner.js added

---
+++ banner.js
@@ -0,0 +1,11 @@
+// banner.js
+const figlet = require("figlet");
+
+figlet("Stock Vision", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
